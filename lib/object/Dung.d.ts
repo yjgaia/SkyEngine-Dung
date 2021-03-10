4d02daf153d957474bfa9fc26bdb2cfb0561d41a
hanul <hanul@hanul.me>
2021-03-10 10:36:05 +0900

    dung in dev

--- lib/object/Dung.d.ts
+++ lib/object/Dung.d.ts
@@ -1,4 +1,6 @@
 import { GameNode } from "@hanul/skyengine";
 export default class Dung extends GameNode {
+    private image;
+    constructor();
 }
 //# sourceMappingURL=Dung.d.ts.map
--- src/object/Dung.ts
+++ src/object/Dung.ts
@@ -1,10 +1,20 @@
-import { GameNode, Image } from "@hanul/skyengine";
+import { GameNode, Image, Rect } from "@hanul/skyengine";
 
 export default class Dung extends GameNode {
 
     private image: Image = new Image({ src: "dung.png" });
 
     constructor() {
+        super({
+            colliders: [new Rect({ width: 40, height: 40 })],
+        });
 
+        this.moveDown({
+            speed: 0,
+            accel: 1000,
+            toY: 235,
+        }, () => {
+            //TODO:
+        });
     }
 }
