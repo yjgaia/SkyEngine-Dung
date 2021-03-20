79d07a57144ef2610cb538dade05c8424321cea1
hanul <hanul@hanul.me>
2021-03-20 15:51:36 +0900

    add bgm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { BodyNode } from "@hanul/skynode";
-import { Fullscreen } from "@hanul/skyengine";
+import { Fullscreen, BGM } from "@hanul/skyengine";
+
+new BGM({ mp3: "bgms/main_screen_bgm.mp3" }).play();
 
 const screen = new Fullscreen({
     width: 360,
